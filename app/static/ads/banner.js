ce85bddb6c4ce44409fe3c3e2f8a055ff2f000eb
Meet Khunt <meetkhunt9913@gmail.com>
2025-11-11 18:37:54 +0000

    Update Content Security Policy in nginx configuration to allow scripts from additional domains

---
+++ app/static/ads/banner.js
@@ -0,0 +1,12 @@
+    // SIDE BANNER 
+
+    (function (jqj) {
+        var d = document,
+            s = d.createElement('script'),
+            l = d.scripts[d.scripts.length - 1];
+        s.settings = jqj || {};
+        s.src = "\/\/tatteredpassenger.com\/bOX\/VXs.dyGzlW0BY\/Wacb\/teTm\/9\/ucZDUxl-kOPJTQYv3\/MNDTAV1PM-DtkqtxNljTcEwDMDDGUXxDMBAD";
+        s.async = true;
+        s.referrerPolicy = 'no-referrer-when-downgrade';
+        l.parentNode.insertBefore(s, l);
+    })({})
